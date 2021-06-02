28483b790ea05fc70bbae438054f789232736b4a
Michael Corey Munn <coreymunn3@gmail.com>
2021-06-02 11:24:01 -0400

    Added Notification to Contact Form with pending/error/success states based on request. Changed form state a bit to reflect binded values and to allow for form reset.

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,10 +1,29 @@
 import styles from './contactForm.module.css';
-import { useState } from 'react';
+import Notification from '../components/Notification';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const ContactForm = () => {
-  const [form, setForm] = useState({});
-  console.log(form);
+  const initialFormState = {
+    email: '',
+    name: '',
+    message: '',
+  };
+  const [form, setForm] = useState(initialFormState);
+  const [requestStatus, setRequestStatus] = useState();
+  const [requestError, setRequestError] = useState();
+  // console.log(form);
+
+  useEffect(() => {
+    if (requestStatus === 'success' || requestStatus === 'error') {
+      const timer = setTimeout(() => {
+        setRequestStatus(null);
+        setRequestError(null);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [requestStatus]);
 
   const handleChange = (e) => {
     setForm({
@@ -13,15 +32,46 @@ const ContactForm = () => {
     });
   };
 
+  const sendContactData = async (contactDetails) => {
+    const { data } = await axios.post('/api/contact', contactDetails);
+    console.log(data);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setRequestStatus('pending');
     try {
-      const { data } = await axios.post('/api/contact', form);
-      console.log(data);
+      await sendContactData(form);
+      setRequestStatus('success');
+      setForm(initialFormState);
     } catch (error) {
-      console.log(error);
+      setRequestStatus('error');
+      setRequestError(error.message);
     }
   };
+
+  let notification;
+  if (requestStatus === 'pending') {
+    notification = {
+      status: 'pending',
+      title: 'Sending Message...',
+      message: 'Your message is on its way!',
+    };
+  }
+  if (requestStatus === 'success') {
+    notification = {
+      status: 'success',
+      title: 'Success',
+      message: 'Sent your Message!',
+    };
+  }
+  if (requestStatus === 'error') {
+    notification = {
+      status: 'error',
+      title: 'Error',
+      message: requestError,
+    };
+  }
 
   return (
     <section className={styles.contact}>
@@ -30,21 +80,39 @@ const ContactForm = () => {
         <div className={styles.controls}>
           <div className={styles.control}>
             <label htmlFor='email'>Your Email</label>
-            <input type='email' id='email' required onChange={handleChange} />
+            <input
+              type='email'
+              id='email'
+              required
+              value={form['email']}
+              onChange={handleChange}
+            />
           </div>
           <div className={styles.control}>
             <label htmlFor='name'>Your Name</label>
-            <input type='text' id='name' required onChange={handleChange} />
+            <input
+              type='text'
+              id='name'
+              value={form['name']}
+              required
+              onChange={handleChange}
+            />
           </div>
         </div>
         <div className={styles.control}>
           <label htmlFor='message'>Your Message</label>
-          <textarea id='message' rows='5' onChange={handleChange} />
+          <textarea
+            id='message'
+            rows='5'
+            value={form['message']}
+            onChange={handleChange}
+          />
         </div>
         <div className={styles.actions}>
           <button type='submit'>Send Message</button>
         </div>
       </form>
+      {notification && <Notification notification={notification} />}
     </section>
   );
 };
