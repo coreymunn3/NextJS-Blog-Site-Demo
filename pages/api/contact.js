534b8efdb5708f2a7754bbcd3782213a3ac9d8ec
Michael Corey Munn <coreymunn3@gmail.com>
2021-06-02 10:52:50 -0400

    Integrated MongoDB connection and insertion logic in contact handler function in API.

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
   // if (req.method === 'GET') {
   //   res.status(200).json({ message: 'You are at contact route' });
   // }
@@ -24,7 +26,31 @@ const handler = (req, res) => {
       message,
     };
 
-    res.status(201).json({ status: 'Success', message: newMessage });
+    let client;
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://coreymunn:Sherm@n5@cluster0.kdjja.mongodb.net/blog-site?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: error.message || 'Could Not Connect to DB' });
+    }
+
+    const db = client.db();
+    let result;
+    try {
+      result = await db.collection('messages').insertOne(newMessage);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: error.message || 'Storing Message Failed' });
+      client.close();
+      return;
+    }
+
+    client.close();
+    res.status(201).json({ status: 'Success', message: result.ops });
   }
 };
 
